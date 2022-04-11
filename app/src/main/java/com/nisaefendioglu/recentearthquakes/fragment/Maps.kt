8809d922b427464cf25c9d2b8393727667edb449
nisaefendioglu <nisaefendioglu0@gmail.com>
2022-04-11 17:05:08 +0300

    added maps

---
+++ app/src/main/java/com/nisaefendioglu/recentearthquakes/fragment/Maps.kt
@@ -0,0 +1,8 @@
+package com.nisaefendioglu.recentearthquakes.fragment
+
+import androidx.fragment.app.Fragment
+import com.nisaefendioglu.recentearthquakes.R
+
+class Maps : Fragment(R.layout.maps) {
+
+}
