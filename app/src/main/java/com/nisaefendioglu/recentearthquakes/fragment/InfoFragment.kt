71358a6be7807feafab16911a9706fb7deeac2cf
nisaefendioglu <nisaefendioglu0@gmail.com>
2023-02-22 19:25:37 +0300

    Edit code

--- app/src/main/java/com/nisaefendioglu/recentearthquakes/fragment/InfoFragment.kt
+++ app/src/main/java/com/nisaefendioglu/recentearthquakes/fragment/InfoFragment.kt
@@ -3,5 +3,4 @@ package com.nisaefendioglu.recentearthquakes.fragment
 import androidx.fragment.app.Fragment
 import com.nisaefendioglu.recentearthquakes.R
 
-class InfoFragment : Fragment(R.layout.info) {
-}
+class InfoFragment : Fragment(R.layout.info)
